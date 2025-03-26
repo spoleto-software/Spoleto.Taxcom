374d0af73464bace37a2490fa30bf39af230d8dd
o.nadymov <o.nadymov@spoleto.ru>
2025-03-26 11:06:22 +0300

    Use Polly for retry policy.

--- src/Spoleto.Taxcom/Providers/TaxcomClient.cs
+++ src/Spoleto.Taxcom/Providers/TaxcomClient.cs
@@ -1,4 +1,7 @@
-﻿using Spoleto.RestClient;
+﻿using Microsoft.Extensions.Http;
+using Polly;
+using Polly.Extensions.Http;
+using Spoleto.RestClient;
 using Spoleto.RestClient.Authentication;
 using Spoleto.Taxcom.Options;
 
@@ -16,9 +19,20 @@ namespace Spoleto.Taxcom
         {
         }
 
-        private static HttpClient CreateNewClient(TaxcomOptions taxcomOptions) //todo: should use Polly?
+        private static HttpClient CreateNewClient(TaxcomOptions taxcomOptions)
         {
-            var httpClient = new HttpClient { BaseAddress = new Uri(taxcomOptions.ServiceUrl) };
+            var retryPolicy = HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+
+            var policyHandler = new PolicyHttpMessageHandler(retryPolicy);
+#if NET
+            policyHandler.InnerHandler = new SocketsHttpHandler();
+#else
+            policyHandler.InnerHandler = new HttpClientHandler();
+#endif
+
+            var httpClient = new HttpClient(policyHandler, true) { BaseAddress = new Uri(taxcomOptions.ServiceUrl) };
 
             return httpClient;
         }
